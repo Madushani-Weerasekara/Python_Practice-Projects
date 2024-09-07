ffda5a4362cd262cf1a09657f441790c541c0d6d
Madushani-Weerasekara <mmweerasekara@gmail.com>
2024-09-07 10:35:01 +0530

    Enryption

--- password_manager.py
+++ password_manager.py
@@ -1,7 +1,30 @@
+from cryptography.fernet import Fernet # allow to encrypt text
+
+def load_key():
+    file = open("key.key", "rb") # opening the file in read in bites mode
+    key = file.read() # read the file
+    file.close() # closing the file
+    return key # returning the key
+    
 master_pwd = input("What is your master password? ")
+key = load_key().master_pwd.encode() # convert master_pwd into bites and add it to the key
+fer = Fernet(key) 
+
+# Initializing a encryption module module
+# key + password + text to encrypt = random text
+# rendom text + key + password = text to encrypt
+
+"""
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file: # wb->write in bites mode
+        key_file.write(key)
+write_key()
+"""
+
 
 def view():
-    with open('passwords.txt', 'r') as f:
+    with open('passwords.txt', 'r') as f: # r-> read mode
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
@@ -11,11 +34,8 @@ def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
     
-    with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd + "\n")
-
-    
-        
+    with open('passwords.txt', 'a') as f: # a-> add
+        f.write(name + "|" + str(fer.encrypt(pwd.encode())) + "\n") # Taking the password turning into bytes with dot encode function and encrypting it. And converting the whole thin into string. And store it beside the name.
 
 
 
